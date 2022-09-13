c8c415fd8113c0ee8c24be73f98e975247d58d53
Ilya Kolchin <kolchinilya7@gmail.com>
2022-09-14 00:53:57 +0300

    Подключаем React и настраиваем css в webpack

---
+++ src/components/Counter/Counter.tsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import './Counter.scss'
+
+const Counter = () => {
+  const [counter, setCounter] = useState(0);
+
+  const increment = () => {
+    setCounter(counter + 1);
+  };
+
+  const decrement = () => {
+    setCounter(counter - 1);
+  };
+
+  return (
+    <div>
+      <h1>{counter}</h1>
+      <button onClick={increment}>increment</button>
+      <button onClick={decrement}>decrement</button>
+    </div>
+  );
+};
+
+export default Counter;
--- src/test.ts
+++
@@ -1,5 +0,0 @@
-export function someFn(arg: number): string {
-  console.log("Text from someFn");
-
-  return "";
-}
