f52f9dd05de2a02c86f3e44ccbebf11d2703196e
Ilya <kolchinilya7@gmail.com>
2023-02-19 21:44:45 +0700

    upgrade webpack config, add babel cache

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -28,7 +28,7 @@ export function buildWebpackConfig(options: BuildOptions): Configuration {
 
     plugins: buildPlugins(options),
 
-    devtool: isDev ? 'inline-source-map' : undefined,
+    devtool: isDev ? 'eval-cheap-module-source-map' : undefined,
 
     devServer: isDev ? buildDevServer(options) : undefined
   };
